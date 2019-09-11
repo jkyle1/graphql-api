fe076388fbabc5e2b415705bd7262c1dda53cdee
kyle.smallwood@gmail.com <kyle.smallwood@gmail.com>
2019-09-11 14:23:50 +0100

    gql.go initial commit

--- gql/gql.go
+++ gql/gql.go
@@ -1,1 +1,20 @@
 package gql
+
+import (
+	"fmt"
+
+	"github.com/graphql-go/graphql"
+)
+
+//ExecuteQuery runs GraphQL queries
+func ExecuteQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+
+	if len(result.Errors) > 0 {
+		fmt.Printf("Errors in ExecuteQuery: %v", result.Errors)
+	}
+	return result
+}
